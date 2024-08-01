556600d3ea524d5b3c3e0503e3b984d8b9f9880c
David Havas <github.wrist068@passinbox.com>
2024-08-02 00:37:10 +0200

    Add base code for mak historical parsing

--- price_scraper/price_scraper/spiders/mak.py
+++ price_scraper/price_scraper/spiders/mak.py
@@ -63,6 +63,91 @@ class MakDailySpider(scrapy.Spider):
                 start_date=product['issueDate']
             )
 
+class MakHistoricalSpider(scrapy.Spider):
+    """
+    Scrapes Hungarian Goverment Bonds historical quotes from 2022.01.01
+
+    We can get historical data by generating pdfs for every day and parse it.
+    https://webkincstar.allamkincstar.gov.hu/report-service/report
+    POST data `{"clientCode":"all","reportName":"_20633_arfolyam_mak","language":"hu","report_params":[{"name":"datum","value":"2023-01-02"}]}`
+    """
+
+    name = "mak_historical"
+    start_urls = ["https://www.allampapir.hu/api/network_rate/m/get_papers"]
+
+    report_names = [
+        "_20631_arfolyam_dkj",
+        "_20632_arfolyam_kkj",
+        "_20633_arfolyam_mak",
+        "_20638_arfolyam_pemak",
+        "_20642_arfolyam_start",
+        "_207461_arfolyam_mapp",
+        "_20644_arfolyam_belf_koz",
+        "_20664_arfolyam_omak",
+    ]
+
+    def start_requests(self):
+        today = datetime.date.today()
+        req_year = 2014
+        req_month = 7
+
+        while datetime.date(req_year, req_month, 1) <= today:
+            url = f"https://www.aranykornyp.hu/public/arfolyamok/archivum/{req_year}-{req_month}"
+            yield scrapy.Request(url=url, callback=self.parse)
+            req_month += 1
+            if req_month > 12:
+                req_month = 1
+                req_year += 1
+                
+    def parse(self, response):
+        """
+        Scrapes the available bond types
+        """
+        content = response.json()
+        for bond_type in content['data'].keys():
+            yield scrapy.Request(
+                f"https://www.allampapir.hu/api/network_rate/m/get_prices/{bond_type}",
+                callback=self.parse_type
+            )
+
+
+    def parse_type(self, response):
+        """
+        Scrapes specific bond type
+        """
+        content = response.json()
+        for product in content['data']['data']:
+            bid_pct = product.get('bidPrice', '1').replace(',', '.')
+            # in some cases they return empty string
+            if not bid_pct:
+                bid_pct = '1'
+            bid_pct = float(bid_pct) / 100
+
+            security_type = product['securityType']
+            # simplified notation for this type
+            if security_type == "MÁP Plusz":
+                security_type = "MÁPP"
+
+            # custom price calculation for bonds with market prices
+            if security_type in ['KTV', 'DKJ']:
+                price = bid_pct
+            else:
+                # 0 is represented as integer and non-zero interest is string
+                accrued_interest = product['accruedInterest']
+                if isinstance(accrued_interest, str):
+                    accrued_interest = float(product['accruedInterest'].replace(',', '.'))
+                price = bid_pct + accrued_interest / 100
+
+            long_name = security_type_to_long_name(security_type)
+            yield PortfolioPerformanceHistoricalPrice(
+                file_name=f"{security_type}_{product['name']}",
+                date=product['settleDate'].replace('.', '-'),
+                price=price,
+                symbol=f"{security_type}_{product['name']}",
+                name=f"{long_name} {product['name']}",
+                start_date=product['issueDate']
+            )
+
 def security_type_to_long_name(security_type: str):
     """
     Converts short bond security types to long names
