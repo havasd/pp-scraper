9d2140907626c525585f7f509e4ea00bc6311f09
David Havas <github.wrist068@passinbox.com>
2025-11-13 00:24:09 +0100

    Extend history for scraping in budapest pension fund (#36)

--- price_scraper/price_scraper/spiders/budapest.py
+++ price_scraper/price_scraper/spiders/budapest.py
@@ -19,14 +19,16 @@ class BudapestPFSpider(scrapy.Spider):
 
     def start_requests(self):
         end_date = datetime.date.today()
-        start_date = end_date - datetime.timedelta(days=14)
+        start_date = end_date - datetime.timedelta(days=32)
         start_date = start_date.strftime("%Y%m%d")
         end_date = end_date.strftime("%Y%m%d")
         # pylint: disable=line-too-long
         url = f"https://www.mbhbank.hu/apps/backend/exchange-rate/exchange-rate-voluntary?active=true&secure=true&fromDate={start_date}&toDate={end_date}"
+        print(f"{url}")
         yield scrapy.Request(url=url, callback=self.parse)
         # pylint: disable=line-too-long
         url = f"https://www.mbhbank.hu/apps/backend/exchange-rate/exchange-rate-personal?growth=true&balanced=true&classic=true&fromDate={start_date}&toDate={end_date}"
+        print(f"{url}")
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response: Response, **kwargs: Any):
